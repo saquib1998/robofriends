a38599bf3c947ecd0813cb05c845699740ff8f04
Saquib Ali <saquib1998@gmail.com>
2020-06-01 22:02:15 +0530

    helloo

---
+++ src/containers/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import { robots } from '../robots';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchfield: ''
+		};
+	}
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => this.setState({ robots: users }));
+	}
+	onsearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	};
+	render() {
+		const filteredRobots = this.state.robots.filter((robots) => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		});
+		if (this.state.robots.length === 0) {
+			return <h1 className="tc">Loading</h1>;
+		} else {
+			return (
+				<div className="tc">
+					<h1 className="f1">RoboFriends</h1>
+					<SearchBox searchChange={this.onsearchChange} />
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
+				</div>
+			);
+		}
+	}
+}
+
+export default App;
